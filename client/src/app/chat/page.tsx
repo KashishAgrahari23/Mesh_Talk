cfa4c0decdfeca9aab4450363a6992bd1a2e7dde
KashishAgrahari23 <kashishagrahari23@gmail.com>
2026-03-15 21:45:19 +0530

    added loading component

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { useRouter } from 'next/navigation'
+import Loading from '../components/Loading'
 
 const ChatApp = () => {
+  const {loading , isAuth} = useAppContext()
+  const router = useRouter()
+
+  useEffect(()=>{
+    if(!isAuth && !loading){
+      router.push("/login")
+    }
+  },[isAuth , loading , router])
+  if(loading) return <Loading/>
   return (
     <div>
 chat app 
