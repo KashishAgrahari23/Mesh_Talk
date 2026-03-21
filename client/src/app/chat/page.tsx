784faaff099155868fb8268bfe38cde17824549b
KashishAgrahari23 <kashishagrahari23@gmail.com>
2026-03-21 20:56:09 +0530

    creating chat page UI

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,22 +1,49 @@
 "use client"
-import React, { useEffect } from 'react'
-import { useAppContext } from '../context/AppContext'
+import React, { useEffect, useState } from 'react'
+import { useAppContext, User } from '../context/AppContext'
 import { useRouter } from 'next/navigation'
 import Loading from '../components/Loading'
+import ChatSideBar from '../components/ChatSideBar'
+
+export interface Message {
+  _id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt: string;
+}
+
 
 const ChatApp = () => {
-  const {loading , isAuth} = useAppContext()
+  const { loading, isAuth, logoutUser, chats, user: loggedInUser, users, fetchChats, setChats } = useAppContext()
+  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [message, setMessage] = useState("")
+  const [sideBar, setSideBar] = useState(false)
+  const [messages, setMessages] = useState<Message[] | null>(null)
+  const [user, setUser] = useState<User | null>(null)
+  const [showAllUser, setShowAllUser] = useState(false)
+  const [isTyping, setIsTyping] = useState(false)
+  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(null)
+
+
   const router = useRouter()
 
-  useEffect(()=>{
-    if(!isAuth && !loading){
+  useEffect(() => {
+    if (!isAuth && !loading) {
       router.push("/login")
     }
-  },[isAuth , loading , router])
-  if(loading) return <Loading/>
+  }, [isAuth, loading, router])
+  if (loading) return <Loading />
   return (
-    <div>
-chat app 
+    <div className='min-h-screen flex bg-gray-900 text-white relative overflow-hidden'>
+      <ChatSideBar />
     </div>
   )
 }
---
+++ client/src/app/components/ChatSideBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+interface ChatSideBarProps{
+    sidebarOpen:boolean
+    
+}
+
+const ChatSideBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatSideBar
